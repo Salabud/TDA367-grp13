a5006a551df9f003742dda2c259d6d0f46865e2c
daniel <daniel.esteban.lahti@gmail.com>
2025-11-27 11:11:26 +0100

    Refactor AntFactory to use builder pattern, also moved world(s) tick handling to Model class

--- src/main/java/Model/World/World.java
+++ src/main/java/Model/World/World.java
@@ -3,13 +3,11 @@ package Model.World;
 import java.util.ArrayList;
 import java.util.List;
 
-import Model.Ants.Ant;
-import Model.Ants.WorkerAnt;
+import Model.Ants.AntFactory;
 import Model.Colony.AntColony;
 import Model.Colony.ColonyMediator;
 import Model.Colony.ColonyTaskBoard;
 import Model.Entity;
-import Model.EntityType;
 
 public class World {
     private List<Entity>[][] entityGrid;
@@ -32,9 +30,9 @@ public class World {
         ColonyMediator mediator = new ColonyMediator();
         ColonyTaskBoard taskBoard = new ColonyTaskBoard();
         AntColony colony = new AntColony(mediator, taskBoard);
-        Ant ant1 = new WorkerAnt(EntityType.WORKER_ANT,this, 0, 50, 50, mediator);
-        colony.addAnt(ant1);
-        addEntity(ant1);
+        
+        AntFactory factory = AntFactory.getInstance();
+        factory.createWorkerAnt(this, colony, 0, 50, 50, mediator);
     }
 
     public void addEntity(Entity entity){
