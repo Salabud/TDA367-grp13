4fa823d141e374804877cb52ec3c0393c9f5a695
ludvig <ludvighygrell@gmail.com>
2025-12-02 19:24:17 +0100

    Added Saving of Items Started implementing saving of Beings Created new Enum structure for beings and ants

--- src/main/java/model/ants/Ant.java
+++ src/main/java/model/ants/Ant.java
@@ -63,14 +63,4 @@ public abstract class Ant extends Being {
         return obj;
     }
 
-    /**
-     * Create a JSON Object of the entity
-     * @return
-     */
-    @Override
-    public JSONObject toJSON(){
-        JSONObject obj = super.toJSON();
-        //TODO implement after refactoring of Entity
-        return obj;
-    }
 }
